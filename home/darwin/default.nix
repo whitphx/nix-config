068510fb1b9b5cabd70cbefe1fd710c2b7154b72
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2026-05-10 11:49:50 +0900

    Source Homebrew with Nix-first PATH on darwin

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -7,4 +7,12 @@
   programs.zsh.shellAliases.intelzsh = "arch -x86_64 zsh";
 
   programs.git.settings.credential.helper = [ "osxkeychain" ];
+
+  # Source Homebrew for HOMEBREW_PREFIX / MANPATH / INFOPATH, then push
+  # /opt/homebrew/{bin,sbin} to the back of $path so Nix-managed
+  # binaries win lookups (`brew shellenv` prepends them by default).
+  programs.zsh.profileExtra = ''
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    path=("''${(@)path:#/opt/homebrew/(bin|sbin)}" /opt/homebrew/bin /opt/homebrew/sbin)
+  '';
 }
